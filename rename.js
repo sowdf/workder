e6fe00052895fe3423a6f17f714e7907b8b89f8f
caozhihui <caozhihui@4399inc.com>
2019-12-10 08:02:23 +0800

    完成

--- rename.js
+++ rename.js
@@ -1,23 +1,36 @@
 const ns = require('node-sketch');
-const icons = require('./icons')
+const icons = require('./icons');
 
 async function run() {
   const sketch = await ns.read(__dirname + '/test.sketch');
   const page = sketch.pages[0];
-  console.log(sketch);
-  Object.keys(icons).map((type)=>{
+
+  Object.keys(icons).map(type => {
     const productIcons = icons[type];
-    Object.keys(productIcons).map((name)=>{
-      const [finalName,ext] = productIcons[name].split('.');
-      page
-        .getAll('symbolInstance', instance => instance.symbolMaster.name === finalName)
-        .forEach(instance => instance.symbolMaster.name = name);
-    })
+    Object.keys(productIcons).map(name => {
+      const dirs = ['Device', 'Device1', 'Device2', 'Device3'];
+      const [finalName, ext] = productIcons[name].split('.');
+      dirs.map(dir => {
+        const latestName = `${dir}/${finalName}`;
+        /*for(let i=1;i<=10;i++) {
+          const symbol = page.get('symbolMaster', latestName);
+          if (symbol && symbol.name) {
+            symbol.name = `${dir}/${name}`;
+          } else {
+            console.log(`svgName:${latestName},changeName:${dir}/${name}`);
+            break;
+          }
+        }*/
+
+        const instances = page.getAll('symbolMaster', instance => instance.name.trim() === latestName);
+        instances.forEach(instance => (instance.name = `${dir}/${name}`));
+      });
+    });
   });
 
   //Save the result
-  sketch.save('modified-design.sketch')
-    .then(console.log('File saved!!'));
+
+  sketch.save('modified-design' + new Date().getTime() + '.sketch').then(console.log('File saved!!'));
 }
 
 run();
